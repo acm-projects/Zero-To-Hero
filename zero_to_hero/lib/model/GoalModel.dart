f103fee6fd8314f0d60f5137ef29b61d46cc77db
Kanishk Garg <61554323+KanishkGar@users.noreply.github.com>
2022-03-26 15:22:20 -0500

    Added in the models for user and goal

---
+++ zero_to_hero/lib/model/GoalModel.dart
@@ -0,0 +1,32 @@
+class GoalModel{
+  String? gid;
+  String? description;
+  Map<String, bool> activeDays = {"Monday":false, "Tuesday":false, "Wednesday":false, "Thursday":false, "Friday":false, "Saturday":false, "Sunday":false};
+  Map<int, bool> reminders = {};//epoch time, true
+  Map<int, bool> pastGoalDays = {};//epoch time, true/false based on completion
+
+  //constructor
+  GoalModel(this.description, this.activeDays);
+
+  //data from server and create the goalModel
+  factory GoalModel.fromRTDB(String gid, Map<String, dynamic> data){
+    GoalModel temp = GoalModel(
+        data['description'] ?? 'Sample Description',
+        data['activeDays'] ?? {"data[activeDays] was null in GoalMode.fromRTDB": false}
+    );
+    temp.gid = gid;
+    temp.reminders = data['reminders'] ?? {};
+    temp.pastGoalDays = data['pastGoalDays'] ?? {};
+    return temp;
+  }
+
+  //write the data to the server in a map/json format
+  Map<String, dynamic> toMap(){
+    return {
+      'description' : description,
+      'reminders' : reminders,
+      'activeDays' : activeDays,
+      'pastGoalDays' : pastGoalDays
+    };
+  }
+}
--- zero_to_hero/lib/model/UserModel.dart
+++ zero_to_hero/lib/model/UserModel.dart
@@ -1,22 +1,40 @@
 class UserModel{
   String? uid;
-  String? email;
+  int streak = 0;
+  int longestStreak = 0;
+  int totalGoals = 0;
+  int totalGoalsCompleted = 0;
+  //Map<Goal ID, true>
+  Map<String, bool> allGoals = {};
+  //Map<Epoch Time, Map<goal id, true>>
+  Map<int, Map<String, bool>> calendarDays = {};
 
-  UserModel({this.uid, this.email});
+  UserModel(this.uid);
 
   //data from server
-  factory UserModel.fromMap(map){
-    return UserModel(
-      uid: map['uid'],
-      email: map['email']
+  factory UserModel.fromRTDB(String uid, Map<String, dynamic> data){
+    UserModel temp = UserModel(
+      uid
     );
-    }
+    temp.streak = data['streak'] ?? 0;
+    temp.longestStreak = data['longestStreak'] ?? 0;
+    temp.totalGoals = data['totalGoals'] ?? 0;
+    temp.totalGoalsCompleted = data['totalGoalsCompleted'] ?? 0;
+    temp.allGoals = data['allGoals'] ?? {};
+    temp.calendarDays = data['calendarDays'] ?? {};
 
-    //sending data to the server
+    return temp;
+  }
+
+  //sending data to the server
   Map<String, dynamic> toMap(){
     return {
-      'uid': uid,
-      'email': email
+      'streak': streak,
+      'longestStreak': longestStreak,
+      'totalGoals': totalGoals,
+      'totalGoalsCompleted': totalGoalsCompleted,
+      'calendarDays': calendarDays,
+      'allGoals': allGoals
     };
   }
 }
