e3b11d29627adfbd3b23deb5501d684768609482
vaibhav <vaibhav.sriram.03@gmail.com>
2022-03-27 11:44:47 -0500

    notifications

--- zero_to_hero/lib/main.dart
+++ zero_to_hero/lib/main.dart
@@ -1,16 +1,41 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:zero_to_hero/LoginPage.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
+AndroidNotificationChannel channel = AndroidNotificationChannel('high_importance', 'Notifs');
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+
+}
 Future<void> main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  await flutterLocalNotificationsPlugin.
+    resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+    ?.createNotificationChannel(channel);
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true
+  );
   runApp(MyApp());
+
+
 }
+
+
+
 class MyApp extends StatelessWidget {
+
   const MyApp({Key? key}) : super(key: key); //not sure if this is necessary
 
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Main',
@@ -20,4 +45,6 @@ class MyApp extends StatelessWidget {
       home: LoginPage(),
     );
   }
+
+
 }
